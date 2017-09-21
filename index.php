bfb7f26ccbf0d497a87d95d21e88677b2642cc5a
papaguillas <32177233+papaguillas@users.noreply.github.com>
2017-09-21 22:16:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,70 @@
+
 <?php
+/*
+* This file is part of GeeksWeb Bot (GWB).
+*
+* GeeksWeb Bot (GWB) is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3
+* as published by the Free Software Foundation.
+* 
+* GeeksWeb Bot (GWB) is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.  <http://www.gnu.org/licenses/>
+*
+* Author(s):
+*
+* © 2015 Kasra Madadipouya <kasra@madadipouya.com>
+*
+*/
 require 'vendor/autoload.php';
-$date = date('m/d/Y h:i:s a', time());
-$client = new Zelenin\Telegram\Bot\Api('430264617:AAHZFISWs-joVLDofVvHzsrIPEe1qc-67aM'); // Set your access token
-$url = 'tranquil-refuge-60512.herokuapp.com/'; // URL RSS feed
-$update = json_decode(file_get_contents('php://input'));	
+$client = new Zelenin\Telegram\Bot\Api(''); // Set your access token
+$url = ''; // URL RSS feed
+$update = json_decode(file_get_contents('php://input'));
+//your app
 try {
-    date_default_timezone_set('Spain/Madrid');
-    if($update->message->text == '/wow')
+    if($update->message->text == '/email')
     {
-	$response = $client->sendMessage([
-	'chat_id' => $update->message->chat->id,
-	'text' => "Fantastic Baby 🎵"				
-	]);	    
+    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+    	$response = $client->sendMessage([
+        	'chat_id' => $update->message->chat->id,
+        	'text' => "You can send email to : Kasra@madadipouya.com"
+     	]);
     }
-      
-    else
+    else if($update->message->text == '/help')
     {
-      if(preg_match("/.*wow.*/", $update->message->text) || preg_match("/.*Wow.*/", $update->message->text) || 
-	 preg_match("/.*WOW.*/", $update->message->text))
-      		{
+    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+    	$response = $client->sendMessage([
+    		'chat_id' => $update->message->chat->id,
+    		'text' => "List of commands :\n /email -> Get email address of the owner \n /latest -> Get latest posts of the blog 
+    		/help -> Shows list of available commands"
+    		]);
+    }
+    else if($update->message->text == '/latest')
+    {
+    		Feed::$cacheDir 	= __DIR__ . '/cache';
+			Feed::$cacheExpire 	= '5 hours';
+			$rss 		= Feed::loadRss($url);
+			$items 		= $rss->item;
+			$lastitem 	= $items[0];
+			$lastlink 	= $lastitem->link;
+			$lasttitle 	= $lastitem->title;
+			$message = $lasttitle . " \n ". $lastlink;
 			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
 			$response = $client->sendMessage([
 					'chat_id' => $update->message->chat->id,
-					'text' => "Fantastic Baby 🎵"				
-				]);	    
-      		}
+					'text' => $message
+				]);
+    }
+    else
+    {
+    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+    	$response = $client->sendMessage([
+    		'chat_id' => $update->message->chat->id,
+    		'text' => "Invalid command, please use /help to get list of available commands"
+    		]);
     }
 } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
     //echo error message ot log it
     //echo $e->getMessage();
 }
-?>
