dee3ea73a033f58e6114d0c1fc1a08803f567cdf
papaguillas <32177233+papaguillas@users.noreply.github.com>
2017-09-21 22:09:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,64 +6,26 @@ $url = 'tranquil-refuge-60512.herokuapp.com/'; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));	
 try {
     date_default_timezone_set('Spain/Madrid');
-    if($update->message->text == '/open7')
+    if($update->message->text == '/wow')
     {
-	$fp = fopen('a.txt', 'w');
-	fwrite($fp, "51");
-	fclose($fp);
+	$response = $client->sendMessage([
+	'chat_id' => $update->message->chat->id,
+	'text' => "Fantastic Baby 🎵"				
+	]);	    
     }
       
     else
     {
-
-	    	   if(preg_match("/.*wow.*/", $update->message->text) || preg_match("/.*Wow.*/", $update->message->text) || preg_match("/.*WOW.*/", $update->message->text)){
+      if(preg_match("/.*wow.*/", $update->message->text) || preg_match("/.*Wow.*/", $update->message->text) || 
+	 preg_match("/.*WOW.*/", $update->message->text))
+      		{
 			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
 			$response = $client->sendMessage([
 					'chat_id' => $update->message->chat->id,
 					'text' => "Fantastic Baby 🎵"				
 				]);	    
-		 }
-	    
-	    	 if(preg_match("/.*buenas noches.*/", $update->message->text) || preg_match("/.*Buenas noches.*/", $update->message->text) || preg_match("/.*Buenas Noches.*/", $update->message->text)){
-			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-			$response = $client->sendMessage([
-					'chat_id' => $update->message->chat->id,
-					'text' => "buenas noches, sueña con tus bias <3"				
-				]);	    
-		 }
-	    
-	    	  if(preg_match("/.*[gG][rR][aA][cC][iI][aA][sS].*[bB][oO][tT].*/", $update->message->text)){
-			
-			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-			$rann = rand(1,6);
-			if($rann == 1 || $rann == 2){
-			$response = $client->sendMessage([
-					'chat_id' => $update->message->chat->id,
-					'text' => "de nada 👉😉👉"				
-				]);
-			}
-			if($rann == 3 || $rann == 4){
-						$response = $client->sendMessage([
-					'chat_id' => $update->message->chat->id,
-					'text' => "천만에요!"				
-				]);
-			}
-			if($rann == 5 || $rann == 6){
-						$response = $client->sendMessage([
-					'chat_id' => $update->message->chat->id,
-					'text' => "Las gracias a nuestro dios G Dragon, no a mi."				
-				]);
-			}
-		 }
-	    
-	    	 if(preg_match("/.*[aA]bogado.*/", $update->message->text)){
-			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-			$response = $client->sendMessage([
-					'chat_id' => $update->message->chat->id,
-					'text' => "el que tengo aqui colgado"				
-				]);	    
-		 }
-	}
+      		}
+    }
 } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
     //echo error message ot log it
     //echo $e->getMessage();
